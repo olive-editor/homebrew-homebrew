42bf9644d6a4aa45396d8c58abdbfb7cbe6d5ac2
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-09-07 17:14:15 +0000

    opentimelineio: add 0.13 bottle.

--- Formula/opentimelineio.rb
+++ Formula/opentimelineio.rb
@@ -7,6 +7,12 @@ class Opentimelineio < Formula
   license "Apache-2.0"
   head "https://github.com/PixarAnimationStudios/OpenTimelineIO.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/olive-editor/homebrew-homebrew/releases/download/opentimelineio-0.13"
+    sha256                               catalina:     "e73f4d2f7d9e7171b6dd8f6fbef62ac71e9d9be9e2b7db774c04399560b646d2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c579f79b4d5562925ca38d584823353eb144b71ad594f5ea1679875da86ffec5"
+  end
+
   depends_on "cmake" => :build
 
   def install
