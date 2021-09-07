d6efc46eae1abbd01018ff4e419f42a7a76594d5
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-09-07 09:03:53 -0700

    oops

--- Formula/opentimelineio.rb
+++ Formula/opentimelineio.rb
@@ -4,7 +4,7 @@ class Opentimelineio < Formula
   url "https://github.com/PixarAnimationStudios/OpenTimelineIO.git",
     tag: "v0.13",
     revision: "b104129ee01fc25745ab0d7f003c03e6accd6330"
-  version: "0.13"
+  version "0.13"
   license "Apache-2.0"
   head "https://github.com/PixarAnimationStudios/OpenTimelineIO.git", branch: "master"
 
