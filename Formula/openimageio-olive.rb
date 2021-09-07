a4a726f1e3eb72f8b61d8bb7939b2b80dc8f9247
itsmattkc <itsmattkc@gmail.com>
2021-09-07 10:24:04 -0700

    Add OpenImageIO

---
+++ Formula/openimageio-olive.rb
@@ -0,0 +1,87 @@
+class Openimageio < Formula
+  desc "Library for reading, processing and writing images"
+  homepage "https://openimageio.org/"
+  url "https://github.com/OpenImageIO/oiio/archive/v2.2.18.0.tar.gz"
+  sha256 "72597619f09b60cc2afc18f378b40fbec62701112957f43cff162dd9a52a26ce"
+  license "BSD-3-Clause"
+  head "https://github.com/OpenImageIO/oiio.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/(?:Release[._-])?v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_big_sur: "5e17b7c08f795f213d4457a7f7942f17df1eaadbaf64b6036b2bacb1b7798f29"
+    sha256 cellar: :any,                 big_sur:       "263b0577dff6a7834f4de270f6f56fec485c2ee872f39180d90c458da4e0a5f9"
+    sha256 cellar: :any,                 catalina:      "e8a0028c161a4f6b1e814ac362a9f251ced660c76a375524f9a1c20e21115c99"
+    sha256 cellar: :any,                 mojave:        "cddc71a90bb3a720bb1ca7324cb69c9161d42594da8c4668ed7733ee1e0d7d40"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60a3a661966a15c3786c510069516c182bba34e92256065f1c7eec184fd46fda"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "boost-python3"
+  depends_on "freetype"
+  depends_on "giflib"
+  depends_on "imath"
+  depends_on "jpeg"
+  depends_on "libheif"
+  depends_on "libpng"
+  depends_on "libraw"
+  depends_on "libtiff"
+  depends_on "opencolorio"
+  depends_on "openexr"
+  depends_on "pybind11"
+  depends_on "python@3.9"
+  depends_on "webp"
+
+  def install
+    args = std_cmake_args + %w[
+      -DCCACHE_FOUND=
+      -DEMBEDPLUGINS=ON
+      -DUSE_FIELD3D=OFF
+      -DUSE_JPEGTURBO=OFF
+      -DUSE_NUKE=OFF
+      -DUSE_OPENCV=OFF
+      -DUSE_OPENGL=OFF
+      -DUSE_OPENJPEG=OFF
+      -DUSE_PTEX=OFF
+      -DUSE_QT=OFF
+    ]
+
+    # CMake picks up the system's python shared library, even if we have a brewed one.
+    py3ver = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    py3prefix = if OS.mac?
+      Formula["python@3.9"].opt_frameworks/"Python.framework/Versions/#{py3ver}"
+    else
+      Formula["python@3.9"].opt_prefix
+    end
+
+    ENV["PYTHONPATH"] = lib/"python#{py3ver}/site-packages"
+
+    args << "-DPYTHON_EXECUTABLE=#{py3prefix}/bin/python3"
+    args << "-DPYTHON_LIBRARY=#{py3prefix}/lib/#{shared_library("libpython#{py3ver}")}"
+    args << "-DPYTHON_INCLUDE_DIR=#{py3prefix}/include/python#{py3ver}"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    test_image = test_fixtures("test.jpg")
+    assert_match "#{test_image} :    1 x    1, 3 channel, uint8 jpeg",
+                 shell_output("#{bin}/oiiotool --info #{test_image} 2>&1")
+
+    output = <<~EOS
+      from __future__ import print_function
+      import OpenImageIO
+      print(OpenImageIO.VERSION_STRING)
+    EOS
+    assert_match version.major_minor_patch.to_s, pipe_output(Formula["python@3.9"].opt_bin/"python3", output, 0)
+  end
+end
