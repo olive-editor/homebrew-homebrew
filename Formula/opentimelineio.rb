21e1b7657c28aab33407507f0e37681d2bf025bb
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-09-07 09:00:41 -0700

    altered otio formula

--- Formula/opentimelineio.rb
+++ Formula/opentimelineio.rb
@@ -1,8 +1,12 @@
 class Opentimelineio < Formula
-  desc "Open Source API and interchange format for editorial timeline information."
+  desc "Open Source API and interchange format for editorial timeline information"
   homepage "http://opentimeline.io"
-  license "NOASSERTION"
-  head "https://github.com/PixarAnimationStudios/OpenTimelineIO.git", tag: "v0.13"
+  url "https://github.com/PixarAnimationStudios/OpenTimelineIO.git",
+    tag: "v0.13",
+    revision: "b104129ee01fc25745ab0d7f003c03e6accd6330"
+  version: "0.13"
+  license "Apache-2.0"
+  head "https://github.com/PixarAnimationStudios/OpenTimelineIO.git", branch: "master"
 
   depends_on "cmake" => :build
 
