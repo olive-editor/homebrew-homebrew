6ae697ba7622995e54fb5ed95eb4cb25459a110d
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2021-09-07 09:07:22 -0700

    now that's pedantic

--- Formula/opentimelineio.rb
+++ Formula/opentimelineio.rb
@@ -2,7 +2,7 @@ class Opentimelineio < Formula
   desc "Open Source API and interchange format for editorial timeline information"
   homepage "http://opentimeline.io"
   url "https://github.com/PixarAnimationStudios/OpenTimelineIO.git",
-    tag: "v0.13",
+    tag:      "v0.13",
     revision: "b104129ee01fc25745ab0d7f003c03e6accd6330"
   version "0.13"
   license "Apache-2.0"
